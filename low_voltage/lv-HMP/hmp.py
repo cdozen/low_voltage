fbd5b5b7564a11139d4e2a052740c89c04748b48
Candan Dozen <candan.dozen@cern.ch>
2022-10-21 16:35:09 +0200

    debug visa connection

--- low_voltage/lv-HMP/hmp.py
+++ low_voltage/lv-HMP/hmp.py
@@ -17,12 +17,8 @@ class Channel(object):
 
 
 class HMP(object):
-    rm = visa.ResourceManager(
-        "/usr/lib64/librsvisa.so@ivi"
-    )  # use the default backend(NI) visa shared library.
-    HMP4040 = rm.open_resource(
-        "TCPIP::192.168.1.202::10002::SOCKET"
-    )  # connect to R&S HMP4040 device via TCPIP
+    rm = visa.ResourceManager( "/usr/lib64/librsvisa.so@ivi")  # use the default backend(NI) visa shared library.
+    HMP4040 = rm.open_resource( "TCPIP::192.168.1.202::10002::SOCKET")  # connect to R&S HMP4040 device via TCPIP
     num_of_channels = 5
 
     def __init__(self, name: str, n_channels: int = 5):
